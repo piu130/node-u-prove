bb4ba79d3a4ad92acc472102147b60a7fc5cc9ad
Ramon Boss <ramon.boss@outlook.com>
2017-07-30 21:29:00 +0200

    UProveHash (#1)
    * UProveHash class

---
+++ src/UProveHash.js
@@ -0,0 +1,130 @@
+const crypto = require('crypto')
+
+/**
+ * Constructs a new hash object.
+ */
+class UProveHash {
+  /**
+   * Constructor.
+   * @param {string} [algorithm='sha256'] - See crypto.createHash from NodeJS.
+   */
+  constructor (algorithm = 'sha256') {
+    this._algorithm = algorithm
+    this._size = 0
+    // set first element to 0 for reduce logic
+    this._steps = [0]
+  }
+
+  /**
+   * Updates a byte.
+   * @param {number} byte - Byte to update.
+   * @returns {void} Nothing.
+   */
+  updateByte (byte) {
+    this._size += 1
+    this._steps.push((buff, offset) => buff.writeUInt8(byte, offset))
+  }
+
+  /**
+   * Updates a number.
+   * @param {number} number - Unsigned integer 32 to update.
+   * @returns {void} Nothing.
+   */
+  updateUInt32 (number) {
+    this._size += 4
+    this._steps.push((buff, offset) => buff.writeUInt32BE(number, offset))
+  }
+
+  /**
+   * Updates null.
+   * @returns {void} Nothing.
+   */
+  updateNull () {
+    this.updateUInt32(0)
+  }
+
+  /**
+   * Updates an octet string.
+   * @param {string} octetString - Octet string to update.
+   * @returns {void} Nothing.
+   */
+  updateOctetString (octetString) {
+    const length = Math.ceil(octetString.length / 2)
+    this._size += length
+    this.updateUInt32(length)
+    this._steps.push((buff, offset) => buff.write(octetString, offset, length, 'hex') + offset)
+  }
+
+  /**
+   * Updates a subgroup construction.
+   * @param {Object} group - Subgroup construction.
+   * @param {string} group.p - Field order.
+   * @param {string} group.q - Prime order.
+   * @param {string} group.g - Generator.
+   * @returns {void} Nothing.
+   */
+  updateSubGroup (group) {
+    this.updateOctetString(group.p)
+    this.updateOctetString(group.q)
+    this.updateOctetString(group.g)
+  }
+
+  // @todo
+  // updateECC (group) {
+  //
+  // }
+
+  /**
+   * Updates a list of booleans.
+   * @param {Array<boolean>} list - List of booleans to update.
+   * @returns {void} Nothing.
+   */
+  updateListOfBooleans (list) {
+    this.updateUInt32(list.length)
+    list.forEach(bool => this.updateByte(bool ? 1 : 0))
+  }
+
+  /**
+   * Updates a list of bytes.
+   * @param {Array<number>} list - List of bytes to update.
+   * @returns {void} Nothing.
+   */
+  updateListOfBytes (list) {
+    this.updateUInt32(list.length)
+    list.forEach(this.updateByte, this)
+  }
+
+  /**
+   * Updates a list of octet strings.
+   * @param {Array<string>} list - List of octet strings to update.
+   * @returns {void} Nothing.
+   */
+  updateListOfOctetStrings (list) {
+    this.updateUInt32(list.length)
+    list.forEach(this.updateOctetString, this)
+  }
+
+  /**
+   * Creates the buffer to hash.
+   * @returns {Buffer} Buffer to hash.
+   * @private
+   */
+  _createBuffer () {
+    const buff = Buffer.alloc(this._size)
+    this._steps.reduce((accumulator, func) => func(buff, accumulator))
+    return buff
+  }
+
+  /**
+   * Calculates the digest.
+   * @param {string} encoding - See hash.digest from NodeJS.
+   * @returns {Buffer|string} Digest.
+   */
+  digest (encoding) {
+    const hash = crypto.createHash(this._algorithm)
+    hash.update(this._createBuffer())
+    return hash.digest(encoding)
+  }
+}
+
+module.exports = UProveHash
---
+++ test/UProveHash_test.js
@@ -0,0 +1,91 @@
+const expect = require('chai').expect
+const UProveHash = require('../src/UProveHash')
+const subGroup = require('../src/SubgroupL2048N256')
+
+describe('UProveHash', function () {
+  describe('should update', function () {
+    it('a byte', function () {
+      const hash = new UProveHash('sha256')
+      hash.updateByte(0x01)
+      expect(hash._createBuffer().toString('hex')).to.equal('01')
+    })
+
+    it('an unsigned integer 32', function () {
+      const hash = new UProveHash('sha256')
+      hash.updateUInt32(12)
+      expect(hash._createBuffer().toString('hex')).to.equal('0000000c')
+    })
+
+    it('null', function () {
+      const hash = new UProveHash('sha256')
+      hash.updateNull()
+      expect(hash._createBuffer().toString('hex')).to.equal('00000000')
+    })
+
+    it('an octet string', function () {
+      const hash = new UProveHash('sha256')
+      hash.updateOctetString('0102030405')
+      expect(hash._createBuffer().toString('hex')).to.equal('000000050102030405')
+    })
+
+    it('a list of booleans', function () {
+      const hash = new UProveHash('sha256')
+      hash.updateListOfBooleans([true, false, false])
+      expect(hash._createBuffer().toString('hex')).to.equal('00000003010000')
+    })
+
+    it('a list of bytes', function () {
+      const hash = new UProveHash('sha256')
+      hash.updateListOfBytes([0x01, 0x00, 0x00])
+      expect(hash._createBuffer().toString('hex')).to.equal('00000003010000')
+    })
+  })
+
+  describe('should digest', function () {
+    it('a byte', function () {
+      const hash = new UProveHash('sha256')
+      hash.updateByte(0x01)
+      expect(hash.digest('hex')).to.equal('4bf5122f344554c53bde2ebb8cd2b7e3d1600ad631c385a5d7cce23c7785459a')
+    })
+
+    it('null', function () {
+      const hash = new UProveHash('sha256')
+      hash.updateNull()
+      expect(hash.digest('hex')).to.equal('df3f619804a92fdb4057192dc43dd748ea778adc52bc498ce80524c014b81119')
+    })
+
+    it('an octet string', function () {
+      const hash = new UProveHash('sha256')
+      hash.updateOctetString('0102030405')
+      expect(hash.digest('hex')).to.equal('16df7d2d0c3882334fe0457d298a7b2413e1e5b7a880f0b5ec79eeeae7f58dd8')
+    })
+
+    it('a subgroup', function () {
+      const hash = new UProveHash('sha256')
+      hash.updateSubGroup(subGroup)
+      expect(hash.digest('hex')).to.equal('7b36c8a3cf1552077e1cacb365888d25c9dc54f3faed7aff9b11859aa8e4ba06')
+    })
+
+    // @todo
+    // it('an ecc', function () {
+    //   const hash = new UProveHash('sha256')
+    //   hash.updateECC(ecc)
+    //   expect(hash.digest('hex')).to.equal('02bb879cb2f89c19579105be662247db15ab45875cfc63a58745361d193ba248')
+    // })
+
+    it('a custom list', function () {
+      const hash = new UProveHash('sha256')
+      hash.updateUInt32(3) // length 3
+      hash.updateByte(0x01)
+      hash.updateOctetString('0102030405')
+      hash.updateNull()
+      expect(hash.digest('hex')).to.equal('dfd6a31f867566ffeb6c657af1dafb564c3de74485058426633d4b6c8bad6732')
+    })
+
+    it('a list of booleans', function () {
+      const hash = new UProveHash('sha256')
+      hash.updateListOfBooleans([true, false, false])
+      expect(hash.digest('hex')).to.equal('401a2e9063adda8a3f68ed55b4f0b597bef0a8ff4e3ecf235f66f9a65e8d773b')
+    })
+  })
+})
