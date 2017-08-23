076a74ae338049d1c4152f82c2a5e79d9fcbff16
Ramon Boss <ramon.bossooutlook.com>
2017-08-23 09:50:24 +0200

    computeXt

---
+++ src/computeXt.js
@@ -0,0 +1,15 @@
+const UProveHash = require('./UProveHash')
+
+/**
+ * Computes Xt.
+ * @param {IssuerParameters} IP - Issuer parameters.
+ * @param {string} TI - Token information field.
+ * @returns {string} Xt value.
+ */
+module.exports = (IP, TI) => {
+  const hash = new UProveHash(IP.UIDh)
+  hash.updateByte(0x01)
+  hash.updateOctetString(IP.hash)
+  hash.updateOctetString(TI)
+  return hash.digestZq(IP.descGq.q)
+}
---
+++ test/computeXt_test.js
@@ -0,0 +1,11 @@
+const {expect} = require('chai')
+const computeXt = require('../src/computeXt')
+const fxtIssuerParameters = require('./fixtures/issuerParameters')
+const {TI} = require('./fixtures/data')
+
+describe('computeXt', function () {
+  it('compute correct value', function () {
+    expect(computeXt(fxtIssuerParameters, TI))
+      .to.equal('721c77bf383bb3402ab49794d285a7cbcfe96e6a593baf3c96ec1cd8f4eadd8d')
+  })
+})
