32a5d2a630b25d5c33b5e6a739f97c99658ac02b
Ramon Boss <ramon.boss@outlook.com>
2018-03-12 17:39:36 +0100

    init issuer and prover

---
+++ src/Issuer.js
@@ -0,0 +1,49 @@
+const {
+  computeXt,
+  computeX,
+  computeGamma,
+  computeSigmaZ,
+  computeSigmaA,
+  computeSigmaB,
+  computeSigmaR
+} = require('functions')
+
+class Issuer {
+  constructor (IP, {attributes, TI}) {
+    this.IP = IP
+    const xt = computeXt(this.IP, TI)
+    const x = []
+    for (let i = 0; i < attributes.length; i++) x.push(computeX(this.IP, this.IP.e[i], attributes[i]))
+    this.gamma = computeGamma(this.IP, x, xt)
+    // this.y0 = // generate at random from Zq
+    this.sigmaZ = computeSigmaZ(this.IP.descGq, this.gamma, this.y0)
+  }
+
+  /**
+   * Generates the first message.
+   * @returns {{sigmaZ: *, sigmaA, sigmaB}} First message.
+   */
+  generateFirstMessage () {
+    // this.w = // generate w at random from Zq
+    const sigmaA = computeSigmaA(this.IP.descGq, this.w)
+    const sigmaB = computeSigmaB(this.IP.descGq, this.gamma, this.w)
+    return {
+      sigmaZ: this.sigmaZ,
+      sigmaA,
+      sigmaB
+    }
+  }
+
+  /**
+   * Generates the third message.
+   * @returns {{sigmaR}} Third message.
+   */
+  generateThirdMessage () {
+    // Get sigma c from first message
+    const sigmaR = computeSigmaR(this.IP, this.sigmaC, this.y0, this.w)
+    delete this.w
+    return {sigmaR}
+  }
+}
+
+module.exports = Issuer
---
+++ src/Prover.js
@@ -0,0 +1,61 @@
+const {
+  computeXt,
+  computeX,
+  computeGamma,
+  computeH,
+  computeT1,
+  computeT2,
+  computeAlphaInverse,
+  computeSigmaZPrime,
+  computeSigmaAPrime,
+  computeSigmaBPrime,
+  computeSigmaCPrime,
+  computeSigmaC
+} = require('./functions')
+
+/**
+ *
+ */
+class Prover {
+  /**
+   *
+   * @param {IssuerParameters} IP - Issuer parameters.
+   * @param {string} TI - Token information field. (eg. expiry dates, token usage restrictions, and token metadata.)
+   * @param applicationAttributes - (eg. the time and date of the interaction, the actions taken by the Verifier upon accepting the U-Prove token, and the Prover's identifier in the Verifier's domain)
+   */
+  constructor (IP, {attributes, TI}) {
+    this.IP = IP
+    this.TI = TI
+    const xt = computeXt(IP, TI)
+    const x = []
+    for (let i = 0; i < attributes.length; i++) x.push(computeX(IP, IP.e[i], attributes[i]))
+    this.gamma = computeGamma(IP, x, xt)
+  }
+
+  /**
+   * Generates the second message.
+   * @returns {{sigmaC}} Second message.
+   */
+  generateSecondMessage () {
+    // this.alpha = // generate at random from Zq*
+    // this.beta1 = // generate at random from Zq
+    // this.beta2 = // generate at random from Zq
+    this.h = computeH(this.IP.descGq, this.gamma, this.alpha)
+    this.t1 = computeT1(this.IP, this.beta1, this.beta2)
+    this.t2 = computeT2(this.IP.descGq, this.h, this.beta2)
+    this.alphaInverse = computeAlphaInverse(this.IP.descGq, this.alpha)
+
+    // Get sigma z from first message
+    this.sigmaZPrime = computeSigmaZPrime(this.IP.descGq, this.sigmaZ, this.alpha)
+    // Get sigma a from first message
+    this.sigmaAPrime = computeSigmaAPrime(this.IP.descGq, this.t1, this.sigmaA)
+    // Get sigma b from first message
+    this.sigmaBPrime = computeSigmaBPrime(this.IP.descGq, this.sigmaZPrime, this.beta1, this.t2, this.sigmaB, this.alpha)
+    this.sigmaCPrime = computeSigmaCPrime(this.IP, this.h, this.PI, this.sigmaZPrime, this.sigmaAPrime, this.sigmaBPrime)
+    const sigmaC = computeSigmaC(this.IP.descGq, this.sigmaCPrime, this.beta1)
+
+    return {sigmaC}
+  }
+}
+
+module.exports = Prover
