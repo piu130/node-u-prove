2ae41ee03e7fab4111adf1bec69904f862510174
Ramon Boss <ramon.boss@outlook.com>
2018-03-12 17:40:40 +0100

    init token

---
+++ src/UProveToken.js
@@ -0,0 +1,28 @@
+/**
+ * Constructs a new token.
+ */
+class UProveToken {
+  /**
+   * Constructor.
+   * @param {string} UIDp - Octet string holding an application-specific unique identifier for the issuer parameters, the value of which MUST be unique across the application realm.
+   * @param {BigInteger} h - U-Prove tokens public key.
+   * @param {string} TI - Token information field. (eg. expiry dates, token usage restrictions, and token metadata.)
+   * @param {string} PI - Prover information field.
+   * @param {BigInteger} sigmaZPrime
+   * @param {BigInteger} sigmaCPrime
+   * @param {BigInteger} sigmaRPrime
+   * @param {boolean} [d=false] - Device protection flag.
+   */
+  constructor (UIDp, h, TI, PI, sigmaZPrime, sigmaCPrime, sigmaRPrime, d = false) {
+    this.UIDp = UIDp
+    this.h = h
+    this.TI = TI
+    this.PI = PI
+    this.sigmaZPrime = sigmaZPrime
+    this.sigmaCPrime = sigmaCPrime
+    this.sigmaRPrime = sigmaRPrime
+    this.d = d
+  }
+}
+
+module.exports = UProveToken
